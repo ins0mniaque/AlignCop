cb8ae620f443d9c9045ba4efda2164827fa486ae
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2024-07-10 13:01:41 -0400

    Add projects

---
+++ tests/AlignCop.Analyzers.Test/AlignCopAnalyzersUnitTests.cs
@@ -0,0 +1,61 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using VerifyCS = AlignCop.Analyzers.Test.CSharpCodeFixVerifier<
+    AlignCop.Analyzers.AlignCopAnalyzersAnalyzer,
+    AlignCop.Analyzers.AlignCopAnalyzersCodeFixProvider>;
+
+namespace AlignCop.Analyzers.Test
+{
+    [TestClass]
+    public class AlignCopAnalyzersUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class {|#0:TypeName|}
+        {   
+        }
+    }";
+
+            var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TYPENAME
+        {   
+        }
+    }";
+
+            var expected = VerifyCS.Diagnostic("AlignCopAnalyzers").WithLocation(0).WithArguments("TypeName");
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+        }
+    }
+}
